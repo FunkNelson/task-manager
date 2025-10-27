d91848a70d945b944554db948b5ad59812bf1d4a
Morgan Ross <rossmb@gmail.comm>
2025-10-27 15:56:41 -0700

    add new options

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ import task_manager_functions as tmf
 task_file = "task_file.txt"
 completed_task_file = "completed_tasks.txt"
 
-version_number = 1.0
+version_number = 1.10
 tmf.display_title(version_number)
 session_active = True
 
-options = [(1, 'Enter a new task'),
-           (2, 'Mark a task complete'),
-           (3, 'Display pending tasks'),
-           (4, 'Display completed tasks'),
-           (5, 'Exit program')]
+options = [(1, 'Enter a new one-time task'),
+           (2, 'Enter a new recurring task'),
+           (3, 'Mark a one-time task complete'),
+           (4, 'Mark a recurring task complete'),
+           (5, 'Display pending one-time tasks'),
+           (6, 'Display pending recurring tasks'),
+           (7, 'Display completed tasks'),
+           (8, 'Exit program')]
 
 while session_active:
     tmf.display_options(options)
@@ -22,16 +25,22 @@ while session_active:
             tmf.add_task(task_file, f"\n{new_task}")
             input('Press Enter to continue...')
         case 2:
+            print("Feature in development")
+        case 3:
             print("\nCURRENT TASK | DATE ENTERED")
             tmf.display_all_tasks(task_file)
             completed_task = int(input("Which task have you completed: "))
             tmf.remove_task(task_file, completed_task, completed_task_file)
             input('Press Enter to continue...')
-        case 3:
+        case 4:
+            print("Feature in development")
+        case 5:
             print("\nCURRENT TASK | DATE ENTERED")
             tmf.display_all_tasks(task_file)
             input('Press Enter to continue...')
-        case 4:
+        case 6:
+            print("Feature in development")
+        case 7:
             print("\nCOMPLETED TASK | DATE ENTERED | DATE COMPLETED")
             tmf.display_all_tasks(completed_task_file)
             input('Press Enter to continue...')
